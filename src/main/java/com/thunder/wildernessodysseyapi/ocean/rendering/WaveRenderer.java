5e7b52ddf4e360c02a3e99a17278ee7c3cf8f73b
Thunderrock424242 <masonjoshua8@gmail.com>
2025-01-04 17:19:41 -0500

    fixed Uv2 to setlight

--- src/main/java/com/thunder/wildernessodysseyapi/ocean/rendering/WaveRenderer.java
+++ src/main/java/com/thunder/wildernessodysseyapi/ocean/rendering/WaveRenderer.java
@@ -74,28 +74,28 @@ public class WaveRenderer {
                 .addVertex(pose, -1.0F, 0.0F, -1.0F)
                 .setColor(255, 255, 255, (int) (255 * foamAlpha))
                 .setUv(0.0F, 0.0F)
-                .setUv2(light)
+                .setLight(light)
                 .endVertex();
 
         vertexConsumer
                 .addVertex(pose, 1.0F, 0.0F, -1.0F)
                 .setColor(255, 255, 255, (int) (255 * foamAlpha))
                 .setUv(1.0F, 0.0F)
-                .setUv2(light)
+                .setLight(light)
                 .endVertex();
 
         vertexConsumer
                 .addVertex(pose, 1.0F, 0.0F, 1.0F)
                 .setColor(255, 255, 255, (int) (255 * foamAlpha))
                 .setUv(1.0F, 1.0F)
-                .setUv2(light)
+                .setLight(light)
                 .endVertex();
 
         vertexConsumer
                 .addVertex(pose, -1.0F, 0.0F, 1.0F)
                 .setColor(255, 255, 255, (int) (255 * foamAlpha))
                 .setUv(0.0F, 1.0F)
-                .setUv2(light)
+                .setLight(light)
                 .endVertex();
 
         bufferSource.endBatch();
