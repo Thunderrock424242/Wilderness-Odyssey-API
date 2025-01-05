990234b0a22ce41e0fce6526fe2b686f79e7fc24
Thunderrock424242 <masonjoshua8@gmail.com>
2025-01-04 21:59:27 -0500

    mod runs but has funny bugs

--- src/main/java/com/thunder/wildernessodysseyapi/ocean/ModClientSetup.java
+++ src/main/java/com/thunder/wildernessodysseyapi/ocean/ModClientSetup.java
@@ -4,10 +4,13 @@ import com.mojang.blaze3d.vertex.PoseStack;
 import com.thunder.wildernessodysseyapi.ocean.rendering.WaveRenderer;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.DeltaTracker;
+import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 import net.neoforged.neoforge.client.event.RenderLevelStageEvent;
+
+import static com.thunder.wildernessodysseyapi.WildernessOdysseyAPIMainModClass.MOD_ID;
 
 @EventBusSubscriber
 public class ModClientSetup {
@@ -15,9 +18,14 @@ public class ModClientSetup {
     /**
      * Handles client setup logic, such as initializing shaders.
      */
-    @SubscribeEvent
-    public static void onClientSetup(FMLClientSetupEvent event) {
-        WaveRenderer.initializeShader(); // Initialize wave-related shaders if needed
+    @EventBusSubscriber(modid = MOD_ID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents {
+
+
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+            WaveRenderer.initializeShader(); // Initialize wave-related shaders if needed
+        }
     }
 
     /**
