7ef0e38b790d3debf364640ec998f3a3ed5669bd
Thunderrock424242 <masonjoshua8@gmail.com>
2025-03-08 16:47:05 -0500

    fixed some client errors also removed fog effects since thats shader packs jobs

--- src/main/java/com/thunder/wildernessodysseyapi/Cloak/ClientEventHandler.java
+++ src/main/java/com/thunder/wildernessodysseyapi/Cloak/ClientEventHandler.java
@@ -3,13 +3,14 @@ package com.thunder.wildernessodysseyapi.Cloak;
 import net.minecraft.client.Minecraft;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.client.event.ClientTickEvent;
 
 @EventBusSubscriber
 public class ClientEventHandler {
     @SubscribeEvent
-    public static void onRenderTick(RenderTickEvent event) {
+    public static void onClientTick(ClientTickEvent.Post event) { // Use Post to run after game logic updates
         Minecraft mc = Minecraft.getInstance();
-        if (mc.player != null && event.phase == RenderTickEvent.Phase.END) {
+        if (mc.player != null) {
             CloakRenderHandler.captureBehindView(mc, mc.player);
         }
     }
