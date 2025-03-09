1a9b836e5ac64205fcd0c01e5df022ef64a58188
Thunderrock424242 <masonjoshua8@gmail.com>
2025-03-09 00:51:14 -0500

    added a little message in nova api. also added jarJar for world edit and one of our api's

--- src/main/java/com/thunder/wildernessodysseyapi/NovaAPI/NovaAPI.java
+++ src/main/java/com/thunder/wildernessodysseyapi/NovaAPI/NovaAPI.java
@@ -12,3 +12,5 @@ public class NovaAPI {
         ThreadMonitor.stopMonitoring(); // Stop monitoring on game exit
     }
 }
+
+///// once our helper the one we payed fixes our code and improves it lets try to ship nova api as a seperate. note for thunder: maybe ask him if we can add him as a contributer on curseforge for his hard work.
