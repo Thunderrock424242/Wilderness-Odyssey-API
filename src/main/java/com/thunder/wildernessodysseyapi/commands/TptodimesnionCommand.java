1948e1b799b14561136e95a87a87092ab96b1f66
Thunderrock424242 <masonjoshua8@gmail.com>
2025-03-10 00:57:51 -0400

    gonna move the telaport command to its own home

--- src/main/java/com/thunder/wildernessodysseyapi/commands/TptodimesnionCommand.java
+++ src/main/java/com/thunder/wildernessodysseyapi/commands/TptodimesnionCommand.java
@@ -1,45 +1,89 @@
 package com.thunder.wildernessodysseyapi.commands;
 
-import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import com.mojang.brigadier.arguments.StringArgumentType;
+import com.mojang.brigadier.context.CommandContext;
 import com.thunder.wildernessodysseyapi.tpdim.TPProcedure;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
 import net.minecraft.commands.arguments.DimensionArgument;
 import net.minecraft.commands.arguments.EntityArgument;
 import net.minecraft.commands.arguments.coordinates.BlockPosArgument;
-import net.minecraft.core.Direction;
+import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.level.Level;
+import net.minecraft.world.level.biome.Biome;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
-import net.neoforged.neoforge.common.util.FakePlayerFactory;
 import net.neoforged.neoforge.event.RegisterCommandsEvent;
+
+import java.util.Optional;
 
 @EventBusSubscriber
 public class TptodimesnionCommand {
-    public TptodimesnionCommand() {
-    }
 
     @SubscribeEvent
     public static void registerCommand(RegisterCommandsEvent event) {
-        event.getDispatcher().register((LiteralArgumentBuilder)((LiteralArgumentBuilder) Commands.m_82127_("tptodimesnion").requires((s) -> s.m_6761_(4))).then(Commands.m_82129_("target_entity", EntityArgument.m_91466_()).then(Commands.m_82129_("destination", BlockPosArgument.m_118239_()).then(Commands.m_82129_("dimension_id", DimensionArgument.m_88805_()).executes((arguments) -> {
-            Level world = ((CommandSourceStack)arguments.getSource()).getUnsidedLevel();
-            double x = ((CommandSourceStack)arguments.getSource()).m_81371_().m_7096_();
-            double y = ((CommandSourceStack)arguments.getSource()).m_81371_().m_7098_();
-            double z = ((CommandSourceStack)arguments.getSource()).m_81371_().m_7094_();
-            Entity entity = ((CommandSourceStack)arguments.getSource()).m_81373_();
-            if (entity == null && world instanceof ServerLevel _servLevel) {
-                entity = FakePlayerFactory.getMinecraft(_servLevel);
+        event.getDispatcher().register(
+                Commands.literal("tptodimension")
+                        .requires(source -> source.hasPermission(4))
+                        .then(Commands.argument("target_entity", EntityArgument.entity())
+                                .then(Commands.argument("dimension", DimensionArgument.dimension())
+                                        .executes(ctx -> executeCommand(ctx, null, null)) // No biome, no position
+                                        .then(Commands.argument("biome", StringArgumentType.string())
+                                                .executes(ctx -> executeCommand(ctx, StringArgumentType.getString(ctx, "biome"), null)) // Biome, no position
+                                                .then(Commands.argument("destination", BlockPosArgument.blockPos())
+                                                        .executes(ctx -> executeCommand(ctx, StringArgumentType.getString(ctx, "biome"), BlockPosArgument.getLoadedBlockPos(ctx, "destination"))) // Biome + Position
+                                                )
+                                        )
+                                )
+                        )
+        );
+    }
+
+    private static int executeCommand(CommandContext<CommandSourceStack> context, String biomeName, BlockPos targetPos) {
+        try {
+            CommandSourceStack source = context.getSource();
+            Entity targetEntity = EntityArgument.getEntity(context, "target_entity");
+            ServerLevel targetDimension = DimensionArgument.getDimension(context, "dimension");
+
+            // If biome is specified, find a position in that biome
+            if (biomeName != null) {
+                Optional<BlockPos> biomeLocation = findBiomePosition(targetDimension, biomeName);
+                if (biomeLocation.isPresent()) {
+                    targetPos = biomeLocation.get();
+                } else {
+                    source.sendFailure(Component.literal("Biome '" + biomeName + "' not found in dimension!"));
+                    return 0;
+                }
             }
 
-            Direction direction = Direction.DOWN;
-            if (entity != null) {
-                direction = entity.m_6350_();
+            // Ensure we have a position to teleport to
+            if (targetPos == null) {
+                targetPos = targetDimension.getSharedSpawnPos();
             }
 
-            TPProcedure.execute(world, arguments);
-            return 0;
-        })))));
+            TPProcedure.execute(targetEntity, targetDimension, targetPos.getX(), targetPos.getY(), targetPos.getZ());
+            source.sendSuccess(() -> targetEntity.getName().copy().append(" has been teleported!"), true);
+        } catch (Exception e) {
+            context.getSource().sendFailure(Component.literal("Failed to execute teleport command: " + e.getMessage()));
+        }
+        return 1;
+    }
+
+    private static Optional<BlockPos> findBiomePosition(ServerLevel world, String biomeName) {
+        for (int x = -500; x < 500; x += 16) {
+            for (int z = -500; z < 500; z += 16) {
+                BlockPos pos = new BlockPos(x, world.getHeight(), z);
+                Biome biome = world.getBiomeManager().getBiome(pos).value();
+                ResourceKey<Biome> biomeKey = world.registryAccess().registryOrThrow(Biome.BIOME_REGISTRY).getResourceKey(biome).orElse(null);
+
+                if (biomeKey != null && biomeKey.location().toString().contains(biomeName.toLowerCase())) {
+                    return Optional.of(pos);
+                }
+            }
+        }
+        return Optional.empty();
     }
 }
--- src/main/java/com/thunder/wildernessodysseyapi/tpdim/TPProcedure.java
+++ src/main/java/com/thunder/wildernessodysseyapi/tpdim/TPProcedure.java
@@ -1,37 +1,23 @@
 package com.thunder.wildernessodysseyapi.tpdim;
 
-import com.mojang.brigadier.context.CommandContext;
-import com.mojang.brigadier.exceptions.CommandSyntaxException;
-import net.minecraft.commands.CommandSource;
-import net.minecraft.commands.CommandSourceStack;
-import net.minecraft.commands.Commands;
-import net.minecraft.commands.arguments.DimensionArgument;
-import net.minecraft.commands.arguments.EntityArgument;
-import net.minecraft.commands.arguments.coordinates.BlockPosArgument;
-import net.minecraft.core.Vec3i;
-import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.level.LevelAccessor;
-import net.minecraft.world.phys.Vec2;
-import net.minecraft.world.phys.Vec3;
 
 public class TPProcedure {
-    public TPProcedure() {
-    }
 
-    public static void execute(LevelAccessor world, CommandContext<CommandSourceStack> arguments) {
-        try {
-            Vec3i pos = BlockPosArgument.m_118242_(arguments, "destination");
-            if (world instanceof ServerLevel _level) {
-                Commands var10000 = _level.m_7654_().m_129892_();
-                CommandSourceStack var10001 = (new CommandSourceStack(CommandSource.f_80164_, new Vec3((double)pos.m_123341_(), (double)pos.m_123342_(), (double)pos.m_123343_()), Vec2.f_82462_, _level, 4, "", Component.m_237113_(""), _level.m_7654_(), (Entity)null)).m_81324_();
-                String var10002 = DimensionArgument.m_88808_(arguments, "dimension_id").m_46472_().m_135782_().toString();
-                var10000.m_230957_(var10001, "execute in " + var10002 + " run tp " + EntityArgument.m_91452_(arguments, "target_entity").m_5446_().getString() + " ~ ~ ~");
-            }
-        } catch (CommandSyntaxException e) {
-            e.printStackTrace();
+    public static void execute(Entity entity, ServerLevel targetDimension, double x, double y, double z) {
+        if (entity == null || targetDimension == null) {
+            return;
         }
 
+        // Check if the entity is already in the correct dimension
+        if (entity.level() == targetDimension) {
+            entity.teleportTo(x + 0.5, y, z + 0.5);
+        } else {
+            entity.changeDimension(targetDimension, (entityToMove, destinationWorld, direction) -> {
+                entityToMove.teleportTo(x + 0.5, y, z + 0.5);
+                return entityToMove;
+            });
+        }
     }
 }
