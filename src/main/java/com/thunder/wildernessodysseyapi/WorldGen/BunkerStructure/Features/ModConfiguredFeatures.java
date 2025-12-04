35ceebead13090fe2ec959f9293d6d806507eb6c
thunder <119463825+Thunderrock424242@users.noreply.github.com>
2025-12-04 16:24:34 -0500

    Register bunker structures

--- src/main/java/com/thunder/wildernessodysseyapi/WorldGen/BunkerStructure/Features/ModConfiguredFeatures.java
+++ src/main/java/com/thunder/wildernessodysseyapi/WorldGen/BunkerStructure/Features/ModConfiguredFeatures.java
@@ -1,11 +1,8 @@
 package com.thunder.wildernessodysseyapi.WorldGen.BunkerStructure.Features;
-
-
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
-import net.minecraft.world.level.levelgen.feature.Feature;
 import net.minecraft.world.level.levelgen.feature.configurations.NoneFeatureConfiguration;
 
 import java.util.Objects;
@@ -26,7 +23,7 @@ public class ModConfiguredFeatures {
      * The constant CUSTOM_STRUCTURE.
      */
     public static final ConfiguredFeature<?, ?> CUSTOM_STRUCTURE = new ConfiguredFeature<>(
-            Feature.NO_OP, // Replace with your custom feature if available
-            NoneFeatureConfiguration.INSTANCE // Use the appropriate configuration
+            ModFeatures.BUNKER_FEATURE.get(),
+            NoneFeatureConfiguration.INSTANCE
     );
 }
