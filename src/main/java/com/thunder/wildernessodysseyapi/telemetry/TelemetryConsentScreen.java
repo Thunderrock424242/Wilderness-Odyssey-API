721e7ba569387e80c4aaf654bff9cab9f986c584
thunder <119463825+Thunderrock424242@users.noreply.github.com>
2026-01-22 00:04:31 -0500

    Reflow telemetry consent text above buttons

--- src/main/java/com/thunder/wildernessodysseyapi/telemetry/TelemetryConsentScreen.java
+++ src/main/java/com/thunder/wildernessodysseyapi/telemetry/TelemetryConsentScreen.java
@@ -6,6 +6,10 @@ import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
+import net.minecraft.util.FormattedCharSequence;
+
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Simple consent dialog for telemetry sharing.
@@ -13,6 +17,14 @@ import net.minecraft.network.chat.Component;
 public class TelemetryConsentScreen extends Screen {
     private static final int BUTTON_WIDTH = 160;
     private static final int BUTTON_HEIGHT = 20;
+    private static final int TEXT_PADDING = 10;
+    private static final int BUTTON_TOP_MARGIN = 20;
+    private static final int MAX_TEXT_WIDTH = 320;
+
+    private List<FormattedCharSequence> descriptionLines = Collections.emptyList();
+    private int titleY;
+    private int descriptionY;
+    private int buttonY;
 
     public TelemetryConsentScreen() {
         super(Component.translatable("screen.wildernessodysseyapi.telemetry.title"));
@@ -21,18 +33,29 @@ public class TelemetryConsentScreen extends Screen {
     @Override
     protected void init() {
         int centerX = this.width / 2;
-        int buttonY = this.height / 2 + 30;
+        int maxTextWidth = Math.min(this.width - 40, MAX_TEXT_WIDTH);
+        this.descriptionLines = this.font.split(
+                Component.translatable("screen.wildernessodysseyapi.telemetry.description"),
+                maxTextWidth
+        );
+        int descriptionHeight = this.descriptionLines.size() * this.font.lineHeight;
+        int textBlockHeight = this.font.lineHeight + TEXT_PADDING + descriptionHeight;
+        int totalHeight = textBlockHeight + BUTTON_TOP_MARGIN + BUTTON_HEIGHT;
+        int startY = Math.max(20, (this.height - totalHeight) / 2);
+        this.titleY = startY;
+        this.descriptionY = this.titleY + this.font.lineHeight + TEXT_PADDING;
+        this.buttonY = this.descriptionY + descriptionHeight + BUTTON_TOP_MARGIN;
 
         addRenderableWidget(Button.builder(
                         Component.translatable("screen.wildernessodysseyapi.telemetry.accept"),
                         button -> handleChoice(ConsentDecision.ACCEPTED))
-                .bounds(centerX - BUTTON_WIDTH - 10, buttonY, BUTTON_WIDTH, BUTTON_HEIGHT)
+                .bounds(centerX - BUTTON_WIDTH - 10, this.buttonY, BUTTON_WIDTH, BUTTON_HEIGHT)
                 .build());
 
         addRenderableWidget(Button.builder(
                         Component.translatable("screen.wildernessodysseyapi.telemetry.decline"),
                         button -> handleChoice(ConsentDecision.DECLINED))
-                .bounds(centerX + 10, buttonY, BUTTON_WIDTH, BUTTON_HEIGHT)
+                .bounds(centerX + 10, this.buttonY, BUTTON_WIDTH, BUTTON_HEIGHT)
                 .build());
     }
 
@@ -54,14 +77,14 @@ public class TelemetryConsentScreen extends Screen {
     @Override
     public void render(GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTick) {
         this.renderBackground(guiGraphics, mouseX, mouseY, partialTick);
-        guiGraphics.drawCenteredString(this.font, this.title, this.width / 2, this.height / 2 - 40, 0xFFFFFFFF);
-        guiGraphics.drawCenteredString(
-                this.font,
-                Component.translatable("screen.wildernessodysseyapi.telemetry.description"),
-                this.width / 2,
-                this.height / 2 - 15,
-                0xFFDDDDDD
-        );
+        int centerX = this.width / 2;
+        guiGraphics.drawCenteredString(this.font, this.title, centerX, this.titleY, 0xFFFFFFFF);
+        int lineY = this.descriptionY;
+        for (FormattedCharSequence line : this.descriptionLines) {
+            int lineX = centerX - this.font.width(line) / 2;
+            guiGraphics.drawString(this.font, line, lineX, lineY, 0xFFDDDDDD, false);
+            lineY += this.font.lineHeight;
+        }
         super.render(guiGraphics, mouseX, mouseY, partialTick);
     }
 
