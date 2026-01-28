dad64aee3d12df7c65a432970c3062f8d2102337
thunder <119463825+Thunderrock424242@users.noreply.github.com>
2026-01-28 09:55:41 -0500

    Handle neural frame removal and chip set tags

--- src/main/java/com/thunder/wildernessodysseyapi/item/neural/NeuralFrameItem.java
+++ src/main/java/com/thunder/wildernessodysseyapi/item/neural/NeuralFrameItem.java
@@ -1,5 +1,7 @@
 package com.thunder.wildernessodysseyapi.item.neural;
 
+import com.thunder.wildernessodysseyapi.Core.ModDamageTypes;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import top.theillusivec4.curios.api.SlotContext;
@@ -12,6 +14,16 @@ public class NeuralFrameItem extends Item implements ICurioItem {
 
     @Override
     public boolean canUnequip(SlotContext slotContext, ItemStack stack) {
-        return false;
+        return true;
+    }
+
+    @Override
+    public void onUnequip(SlotContext slotContext, ItemStack newStack, ItemStack stack) {
+        LivingEntity wearer = slotContext.entity();
+        if (wearer.level().isClientSide) {
+            return;
+        }
+
+        wearer.hurt(wearer.damageSources().source(ModDamageTypes.NEURAL_FRAME_REMOVAL), Float.MAX_VALUE);
     }
 }
