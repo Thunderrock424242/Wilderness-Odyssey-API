c0e487edc7c1367c59cf3c4513ab6d24bf7fa240
thunder <119463825+Thunderrock424242@users.noreply.github.com>
2026-02-09 17:51:14 -0500

    Center neural frame model on head

--- src/main/java/com/thunder/wildernessodysseyapi/Client/NeuralFrameModel.java
+++ src/main/java/com/thunder/wildernessodysseyapi/Client/NeuralFrameModel.java
@@ -36,7 +36,7 @@ public class NeuralFrameModel extends EntityModel<AbstractClientPlayer> {
                         .addBox(-3.0F, -5.5F, -4.0F, 6.0F, 4.0F, 0.2F, new CubeDeformation(0.0F))
                         .texOffs(0, 5)
                         .addBox(3.0F, -5.5F, -4.0F, 0.4F, 4.0F, 0.2F, new CubeDeformation(0.0F)),
-                PartPose.ZERO
+                PartPose.offset(0.0F, 0.0F, 2.0F)
         );
         return LayerDefinition.create(mesh, 16, 16);
     }
