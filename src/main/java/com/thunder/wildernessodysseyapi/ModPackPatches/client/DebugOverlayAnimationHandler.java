85e8edbca9cce7eb22931ea0944da3c9a36ea021
Thunderrock424242 <masonjoshua8@gmail.com>
2026-02-09 15:26:48 -0500

    made some tick fixes
    Signed-off-by: Thunderrock424242 <masonjoshua8@gmail.com>

--- src/main/java/com/thunder/wildernessodysseyapi/ModPackPatches/client/DebugOverlayAnimationHandler.java
+++ src/main/java/com/thunder/wildernessodysseyapi/ModPackPatches/client/DebugOverlayAnimationHandler.java
@@ -4,16 +4,16 @@ import com.thunder.wildernessodysseyapi.Core.ModConstants;
 import net.minecraft.client.Minecraft;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.fml.common.Mod;
-import net.neoforged.neoforge.event.TickEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.client.event.ClientTickEvent;
 
-@Mod.EventBusSubscriber(modid = ModConstants.MOD_ID, value = Dist.CLIENT)
+@EventBusSubscriber(modid = ModConstants.MOD_ID, value = Dist.CLIENT)
 public final class DebugOverlayAnimationHandler {
     private DebugOverlayAnimationHandler() {
     }
 
     @SubscribeEvent
-    public static void onClientTick(TickEvent.ClientTickEvent event) {
+    public static void onClientTick(ClientTickEvent event) {
         if (event.phase != TickEvent.Phase.END) {
             return;
         }
