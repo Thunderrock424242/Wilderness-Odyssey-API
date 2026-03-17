f45ab59c871000390415c16b51239cde64032a5a
Thunderrock424242 <masonjoshua8@gmail.com>
2026-03-17 16:16:12 -0400

    update
    Signed-off-by: Thunderrock424242 <masonjoshua8@gmail.com>

--- src/main/java/com/thunder/wildernessodysseyapi/bridge/StructureBlockCornerCacheBridge.java
+++ src/main/java/com/thunder/wildernessodysseyapi/bridge/StructureBlockCornerCacheBridge.java
@@ -1,9 +1,10 @@
-package com.thunder.wildernessodysseyapi.mixin;
+package com.thunder.wildernessodysseyapi.bridge;
 
 /**
  * Bridge contract used by mixins to synchronize structure-block corner cache lifecycle hooks
  * without relying on reflective invocation.
  */
+
 public interface StructureBlockCornerCacheBridge {
 
     void wildernessodysseyapi$bridge$syncCornerCache();
