e29e314ac4aba267f5095732d4703b3759e1d5a1
thunder <119463825+Thunderrock424242@users.noreply.github.com>
2026-03-27 19:19:41 -0400

    Define anomaly biomes as datapack worldgen entries

--- src/main/java/com/thunder/wildernessodysseyapi/worldgen/biome/ModBiomes.java
+++ src/main/java/com/thunder/wildernessodysseyapi/worldgen/biome/ModBiomes.java
@@ -5,14 +5,10 @@ import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.biome.Biome;
-import net.neoforged.neoforge.registries.DeferredHolder;
-import net.neoforged.neoforge.registries.DeferredRegister;
 
 public final class ModBiomes {
     private ModBiomes() {
     }
-
-    public static final DeferredRegister<Biome> BIOMES = DeferredRegister.create(Registries.BIOME, ModConstants.MOD_ID);
 
     public static final ResourceKey<Biome> ANOMALY_PLAINS_KEY = ResourceKey.create(Registries.BIOME,
             ResourceLocation.fromNamespaceAndPath(ModConstants.MOD_ID, "anomaly_plains"));
@@ -30,10 +26,4 @@ public final class ModBiomes {
     @Deprecated
     public static final ResourceKey<Biome> ANOMALY_DESERT_KEY = ResourceKey.create(Registries.BIOME,
             ResourceLocation.fromNamespaceAndPath(ModConstants.MOD_ID, "anomaly_desert"));
-
-    public static final DeferredHolder<Biome, Biome> ANOMALY_PLAINS = BIOMES.register("anomaly_plains", AnomalyBiomes::anomalyPlains);
-    public static final DeferredHolder<Biome, Biome> ANOMALY_TUNDRA = BIOMES.register("anomaly_tundra", AnomalyBiomes::anomalyTundra);
-    public static final DeferredHolder<Biome, Biome> ANOMALY_RAINFOREST = BIOMES.register("anomaly_rainforest", AnomalyBiomes::anomalyRainforest);
-    @Deprecated
-    public static final DeferredHolder<Biome, Biome> ANOMALY_ZONE = BIOMES.register("anomaly_zone", AnomalyBiomes::anomalyPlains);
 }
